8d02df26f0d73676a057c61c2258f9ec3c533373
Luydmila <Ludapanova178@gmail.com>
2023-04-08 14:04:02 +0500

    добавла папки zadacha_1, zadacha_2, zadach_3

---
+++ zadacha_3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите массив");
+int[] randomArray = new int[8];
+
+for (int i = 0; i < 8; i++)
+{
+randomArray[i] = new Random().Next(1,99);
+Console.Write(randomArray[i] + " ");
+}
